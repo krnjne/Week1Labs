3deffb4cae4722594402350558ef972d44561880
krnjne <kareeenajane@gmail.com>
2026-08-28 23:37:37 +0800

    Lab 5: Navigation between Screens and AddTasks Screens

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import AddTasksScreen from './screens/AddTasksScreen';
-
+import WelcomeScreen from './screens/WelcomeScreen';
+const Stack = createNativeStackNavigator();
 export default function App() {
-return <AddTasksScreen />;
 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="AddTasks" component={AddTasksScreen} options={{ title: 'My Tasks' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native';
-export default function WelcomeScreen() {
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function WelcomeScreen({ navigation })  {
 
     return (
 
@@ -12,7 +13,7 @@ export default function WelcomeScreen() {
             </View>
 
             <View style={styles.footer}>
-                <Text style={styles.footerText}>Lab 3: Flexbox Layout</Text>
+                <Button title="Check on tasks i guess" onPress={() => navigation.navigate('AddTasks')}/>
             </View>
         </View>
     );
@@ -20,12 +21,11 @@ export default function WelcomeScreen() {
 
 const styles = StyleSheet.create({
 
-    container: { flex: 1, backgroundColor: '#1B2A4A', justifyContent: 'flex-start'},
+    container: { flex: 1, backgroundColor: '#1B2A4A', justifyContent: 'space-between'},
     header: { flex: 1, justifyContent: 'center', alignItems: 'center' },
     emoji: { fontSize: 64, marginBottom: 12 },
     title: { fontSize: 28, fontWeight: 'bold', color: '#FFFFFF' },
     subtitle: { fontSize: 14, color: '#C7D2E8', marginTop: 6 },
     footer: { paddingBottom: 40, alignItems: 'center' },
-    footerText: { color: '#9FB0D0', fontSize: 12 },
 
 });
